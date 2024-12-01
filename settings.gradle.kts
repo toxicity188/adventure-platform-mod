371424f2c48d0589729f697dcc0ad2b264c817f1
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-11-30 23:24:30 -0700

    build: Replace hacky workarounds for run configs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ pluginManagement {
 plugins {
   id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
   id("fabric-loom") version "1.9.2"
-  id("net.neoforged.moddev.repositories") version "1.0.23"
+  id("net.neoforged.moddev.repositories") version "2.0.52-beta"
 }
 
 rootProject.name = "adventure-platform-mod-parent"
